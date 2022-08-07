46cb0e0ffbd883b94fb3acce8ec55cb9033503b3
Maksym Lytovchenko <lmb1988@ukr.net>
2022-08-07 19:15:36 +0300

    hw-6

--- src/js/task-07.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector('input');
-const text = document.querySelector('#text');
-
-input.addEventListener('change', changeSizeText);
-function changeSizeText(event) {
-  text.style.fontSize = event.currentTarget.value + 'px';
-}
