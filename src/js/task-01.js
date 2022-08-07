35d9fff60bf76c14e3c63a1a6bfe27a37629589f
Maksym Lytovchenko <77207925+LITOHA-77@users.noreply.github.com>
2022-08-07 19:18:58 +0300

    Delete src directory

--- src/js/task-01.js
+++
@@ -1,9 +0,0 @@
-const categoryEl = document.querySelector('#categories');
-console.log(`Number of Categories: ${categoryEl.children.length}`);
-
-const itemEl = categoryEl.querySelectorAll('li.item');
-
-itemEl.forEach(item => {
-  console.log(`Category: ${item.querySelector('h2').textContent}`);
-  console.log(`Elements: ${item.querySelectorAll('li').length}`);
-});
