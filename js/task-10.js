3e9cfdeefe67447aa23bcb1eab01be6e3b89d9a0
Maksym Lytovchenko <77207925+LITOHA-77@users.noreply.github.com>
2022-08-08 22:15:33 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,20 +1,36 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-// const input = document.querySelector('input');
-// const btnCreate = document.querySelector('[data-create]');
-// const btnDestroy = document.querySelector('[data-destroy]');
-// input.addEventListener('input', () => {});
-// btnCreate.addEventListener('click', createBoxes);
-// btnDestroy.addEventListener('click', destroyBoxes);
+// Интересная задача, код писал с помощью, сам пока такую логику не потяну, но опыт наше все :)
 
-// function createBoxes(amount) {
-//   console.log('create');
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-// function destroyBoxes() {
-//   console.log('destroy');
-//   // reset();
-// }
+const [input, renderBtn, destroyBtn] =
+  document.querySelector('#controls').children;
 
-// не успеваю постараюсь решить ее после сдачи дз
+const boxContainer = document.querySelector('#boxes');
+
+let sizeDiv = 30;
+
+const callback = () =>
+  +input.value <= +input.getAttribute('max') && +input.value >= 1
+    ? createBoxes(+input.value)
+    : alert('введите число от  1 до 100');
+
+const createBoxes = amount => {
+  let string = '';
+  for (let i = 0; i < amount; i += 1) {
+    string += `<div style="background-color:${getRandomHexColor()}; width:${sizeDiv}px; height:${sizeDiv}px;"></div>`;
+
+    sizeDiv += 10;
+  }
+
+  return boxContainer.insertAdjacentHTML('beforeend', string);
+};
+
+const destroyBoxes = () => {
+  boxContainer.innerHTML = '';
+  input.value = 0;
+  sizeDiv = 30;
+};
+renderBtn.addEventListener('click', callback);
+destroyBtn.addEventListener('click', destroyBoxes);
